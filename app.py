b0c5e26bded60e1109172bb4bc8afea7093a31c8
Agustin-desktop <agustin2051@gmail.com>
2024-09-25 00:09:48 -0300

    mod: video

--- app.py
+++ app.py
@@ -25,4 +25,7 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
+    # local:
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    # prod
     app.run(host="0.0.0.0", port=5000)
